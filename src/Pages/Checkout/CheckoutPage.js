32d05332e26d3688e79c57714c632d98dfc73d41
batuannguyen22 <103261848+batuannguyen22@users.noreply.github.com>
2022-07-30 13:47:19 +0700

    Update CheckoutPage.js

--- src/Pages/Checkout/CheckoutPage.js
+++ src/Pages/Checkout/CheckoutPage.js
@@ -238,7 +238,7 @@ const BookedTicketHistory = () => {
   }, [userInfo]);
 
   const renderTicketItem = () => {
-    return userInfo.thongTinDatVe?.map((ticket, index) => {
+    return userInfo?.thongTinDatVe?.map((ticket, index) => {
       return (
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full" key={index}>
           <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
